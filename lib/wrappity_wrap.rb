eb79ca79a11c3f886dc3b97c7be5ac311b39b146
Tony Griffin <tony@madetech.com>
2019-09-03 15:16:36 +0100

    Add string < column and space before column tests

--- lib/wrappity_wrap.rb
+++ lib/wrappity_wrap.rb
@@ -3,8 +3,19 @@ require "wrappity_wrap/version"
 module WrappityWrap
   class Error < StandardError; end
 
-  def wrap(string, length)
-    ""
+    def wrap(string, column)
+      if string.nil?
+        return ""
+      end
+
+      return string if string.length <= column
+
+      if string[0..column].index(" ") != nil
+          whitespace = string[0...column].rindex(" ")
+         return string[0...whitespace] + "/n" +wrap(string[whitespace+1..-1],column)
+      end
+      ""
+    end
   end
 
-end
+
--- spec/wrappity_wrap_spec.rb
+++ spec/wrappity_wrap_spec.rb
@@ -10,4 +10,16 @@ RSpec.describe WrappityWrap do
     expect(WrappityWrap.wrap(nil, 4)).to eq("")
   end
 
+  it "returns "" when given empty string" do
+    expect(WrappityWrap.wrap("", 4)).to eq("")
+  end
+
+  it "returns string when string is shorter than column" do
+    expect(WrappityWrap.wrap("word", 6)).to eq("word")
+  end
+
+  it "returns string when string has a space before the column" do
+    expect(WrappityWrap.wrap("long word", 5)).to eq("long/nword")
+  end
+
 end
